058a23fe56f0faa51d9b6b54261777bd2b3a2f14
Neckfly <baptiste.mqt91@gmail.com>
2023-02-15 22:32:48 +0100

    last update

--- objective_functions/objective_functions.py
+++ objective_functions/objective_functions.py
@@ -13,22 +13,15 @@ def l21_norm(W):
 
 def get_group_regularization(weights):
     const_coeff = lambda W: torch.sqrt(torch.tensor(W.size(dim=-1), dtype=torch.float32))
-    #return torch.sum(torch.tensor([torch.multiply(const_coeff(W), l21_norm(W)) for name, W in model.named_parameters() if 'bias' not in name]))
     return torch.sum(torch.tensor([torch.multiply(const_coeff(W), l21_norm(W)) for name, W in weights.items() if 'bias' not in name]))
 
 def sparse_group_lasso(weights):
     grouplasso = get_group_regularization(weights)
-    #l1 = torch.linalg.norm(torch.cat([torch.reshape(x[1] ,[-1]) for x in model.named_parameters()], dim=0))
+
     l1 = torch.linalg.norm(torch.cat([torch.reshape(x[1] ,[-1]) for x in weights.items()], dim=0))
     
     sparse_lasso = grouplasso + l1
     return sparse_lasso
-
-'''
-def f1_norm(feat, label, model):
-    cross_entropy_norm = nn.CrossEntropyLoss()(label, model(feat).long())
-    return cross_entropy_norm
-'''
 
 def f1_norm(feat, label, model, criterion):
     return criterion(label, model(feat))
